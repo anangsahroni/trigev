4a80867684c88fd91600c33fb16941aea5d7527a
Anang Sahroni <48075471+anangsahroni@users.noreply.github.com>
2021-09-12 10:38:56 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+from setuptools import setup
+
 setup(
     name='trigev',
     version='0.1.0',
-    packages=find_packages(include=[]),
-    install_requires=[]
+    py_modules=['trigev'],
+    install_requires=[],
+    entry_points='''
+        [console_scripts]
+        trigev=trigev:trigev
+    ''',
 )
